cd54aad6e8f601683e2c32f96d0ad18cd8801a7a
Willy MJ <wmoralesj2@miumg.edu.gt>
2022-10-19 00:24:55 -0600

    Avance de proyecto

--- src/ProyectoUMG/Tablero.java
+++ src/ProyectoUMG/Tablero.java
@@ -4,10 +4,62 @@
  */
 package ProyectoUMG;
 
+import java.io.Serializable;
+
 /**
  *
- * @author acer
+ * @author Willy
  */
-public class Tablero {
+
+//Indicamos que todas las instancias puedan añadirce al vector
+// con "implements Serializable"
+public class Tablero implements Serializable {
     
+    //Atributos
+    
+    private String nombre;
+    private int Id;
+    
+    // Constructor
+    
+    public Tablero(String nombre, int Id) {
+        this.nombre = nombre;
+        this.Id = Id;
+    }
+    
+    //Consultor
+    public String getNombre() {
+        return nombre;
+    }
+
+    //Modificador
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    //Consultor
+    public int getId() {
+        return Id;
+    }
+
+    
+    //Modificador
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+    
+    // Metodo analizador
+    // podemos agregar atributos para imprimir las instancias
+    public String toString(){
+        return "Nombre del tablero " + this.Id + "-"+ this.nombre;
+    }
+   
+            
+            
+            
+            
+            
+            
+            
 }
